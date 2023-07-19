1ca779bc195af93bc7572e10c55772c42c9eb6ab
nusraissack <issacknusra05@gmail.com>
2023-07-20 01:35:20 +0300

    update

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ document.querySelector('.hamburger-menu').addEventListener('click',()=>{
   document.querySelector('.container').classList.toggle('change');
 });
 
-document.querySelector('.scroll-btn').addEventListener('')
+document.querySelector('.scroll-btn').addEventListener('click', ()=>{
+  document.querySelector('html').style.scrollBehavior='smooth';
+  setTimeout(()=>{
+    document.querySelector('html').style.scrollBehavior='unset';
+  }, 1000)
+})
